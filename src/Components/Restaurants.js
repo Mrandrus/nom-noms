6842e8fc6fd72b6ff9281a7f061478845e5d08cb
r jones <r.jones@colostate.edu>
2023-06-09 12:27:25 -0600

    adding cuisine selector css module

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Restaurants.module.css'
+/* == REACT ROUTER DOM == */
+import { BrowserRouter, Router, Routes, Route, Link, useNavigate } from 'react-router-dom'
 
 /* == IMPORT RAW DATA == */
 import jsonReviewData from '../Reviews/foodReviewJS'
 
 const Restaurants = () => {
+  const [activeCuisine, setActiveCuisine] = useState(null)
+  const cuisineList = removeDuplicatesByKey(jsonReviewData, 'Cuisine')
   return (
     <div className={styles.restaurantsContainer}>
-      <RestaurantCuisineFilter />
+      <RestaurantCuisineFilter {...{ cuisineList, activeCuisine, setActiveCuisine }} />
       <RestaurantListByCuisine />
     </div>
   )
@@ -15,11 +19,33 @@ const Restaurants = () => {
 
 export default Restaurants
 
+/**
+ *
+ *
+ * ===================== CHILD COMPONENTS =====================
+ */
+
 const RestaurantCuisineFilter = props => {
-  const { cuisines, activeCuisine } = props
+  const { cuisineList, activeCuisine, setActiveCuisine } = props
   return (
-    <div>
-      <h1>RestaurantCuisineFilter</h1>
+    <div className="restaurant-sideBar">
+      <ul>
+        {getArray(cuisineList).map((p, i) => (
+          <Link to="/Restaurants" key={i}>
+            <li
+              key={p.Id}
+              className={`${activeCuisine === p.Cuisine ? styles.activeCuisine : ''}`}
+            >
+              <button
+                className="restaurant-sideBar-buttons"
+                onClick={() => setActiveCuisine(p.Cuisine)}
+              >
+                {p.Cuisine}
+              </button>
+            </li>
+          </Link>
+        ))}
+      </ul>
     </div>
   )
 }
@@ -31,4 +57,14 @@ const RestaurantListByCuisine = props => {
       <h1>RestaurantListByCuisine</h1>
     </div>
   )
+}
+
+/* === HELPERS === */
+function getArray(data) {
+  return Array.isArray(data) === true ? data : []
+}
+
+function removeDuplicatesByKey(arr, key) {
+  const data = [...new Map(arr.map(item => [item[key], item])).values()]
+  return data
 }
