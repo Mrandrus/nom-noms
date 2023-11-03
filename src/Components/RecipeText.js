18eb800511cd57b0ae151d9d17884e08f4e037fb
matt andrus <mattrandrus@gmail.com>
2023-11-03 00:45:17 -0600

    Added spaghetti recipe

--- src/Components/RecipeText.js
+++ src/Components/RecipeText.js
@@ -24,13 +24,17 @@ function RecipeText(props) {
 
   switch (recipeId.props?.Id) {
     case 1:
-      return <DrunkenNoodles recipe={recipeId.props} />
+      return <DrunkenNoodles recipe={recipeId.props} />;
     case 2:
-      return <Gyro />;
+      return <Spaghetti recipe={recipeId.props} />;
     case 3:
       return <Lasagna recipe={recipeId.props} />;
     case 4:
-      return <ShrimpScampi />
+      return <ShrimpScampi />;
+    case 5:
+      return <Gyro />;
+    case 6:
+      return <JalapenoSalsa />;
     default:
       return "";
   }
@@ -51,13 +55,21 @@ const Gyro = () => {
   );
 };
 
+const JalapenoSalsa = () => {
+  return (
+    <div>
+      <h3>Coming Soon</h3>
+    </div>
+  );
+};
+
 const ShrimpScampi = () => {
   return (
     <div>
       <h3>Coming Soon</h3>
     </div>
-  )
-}
+  );
+};
 
 const Lasagna = ({ recipe }) => {
   return (
@@ -147,6 +159,60 @@ const DrunkenNoodles = ({ recipe }) => {
           the green onion, and the thai basil. I recommend tearing the thai
           basil into small pieces and then tossing them in. Mix everything
           together throughly and serve with squeezed lime over top.
+        </p>
+      </div>
+      <div className="recipe-instructions-images">
+        {getArray(
+          recipe?.StepImages.map((p, q) => {
+            return <img src={p} key={q} alt={NomNomsLogo} />;
+          })
+        )}
+      </div>
+    </div>
+  );
+};
+
+const Spaghetti = ({ recipe }) => {
+  return (
+    <div>
+      <div className="recipe-ingredients">
+        <h3>Ingredients</h3>
+        <ul>
+          {getArray(recipe?.Ingredients).map((p, q) => {
+            return <li key={q}>{p}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="recipe-ingredients-image">
+        <img src={recipe.IngredientsImage} alt={`${recipe.Recipe}-image`} />
+      </div>
+      <div className="recipe-instructions">
+        <p>
+          A few of the ingredients missed picture day. Apologies from the
+          kitchen staff.
+        </p>
+        <p>
+          Heat olive oil in dutch oven. Dice full onion, celery, and mince
+          garlic. Add ground italian sausage to dutch oven. Cook, stirring
+          periodically until meat is browned. Remove meat from dutch oven. Add
+          onion and celery, then garlic. Add around a half cup of white wine and
+          stir until liquid mostly evaporates. Stir in one table spoon of tomato
+          paste. Add salt, pepper, 1 tablespoon of sugar, and Italian seasoning.
+          Pour in 2 cans of crushed tomatoes and quart of chicken stock/broth.
+          Add meat back to sauce. Bring to boil then reduce heat and let it
+          simmer for two to three hours until sauce thickens.
+        </p>
+        <p>
+          Bring 4 quarts of salted water to a boil. Add spaghetti or linguine
+          and follow instructions on box for cook time.
+        </p>
+        <p>
+          Before draining pasta add around one cup of water to sauce mixture.
+          Drain noodles. Add fresh or dried parsley to sauce and finely grate
+          parmesan cheese into sauce, I like to do around a cup. Add salt and
+          pepper to taste. You can let sauce simmer for a little longer if
+          necessary in order to thicken, if not serve over noodles with grated
+          parmesan.
         </p>
       </div>
       <div className="recipe-instructions-images">
